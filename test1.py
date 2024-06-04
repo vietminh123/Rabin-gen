132434af5b0e6e4cbe616b154853b81bd4976a1b
vietminh123 <vietminh319@gmail.com>
2024-06-04 22:59:07 +0700

    update

--- test1.py
+++ test1.py
@@ -1,6 +1,6 @@
 import random
 # Hàm tính a^m mod n
-def binPow(a, k, n):
+def powMod(a, k, n):
     result = 1
     a = a % n
     while k:
@@ -12,10 +12,10 @@ def binPow(a, k, n):
  
 # Hàm kiểm tra a^m mod n có đồng dư với -1 hoặc 1 hay không
 def Compute(a, n, k, m):
-    mod = binPow(a, m, n)
+    mod = powMod(a, m, n)
     if mod == 1 or mod == n - 1:
         return True
-    for _ in range (k):
+    for _ in range (k - 1):
         mod = (mod * mod) % n
         if mod == n - 1:
             return True  
@@ -27,9 +27,9 @@ def Miller_Rabin(n):
         return True
     if not(n & 1):
         return False
-    k,m = 0, n - 1
+    k, m = 0, n - 1
     while not(m & 1):
-        k+=1
+        k += 1
         m >>= 1
     loop = 3
     for _ in range (loop):
@@ -38,10 +38,25 @@ def Miller_Rabin(n):
             return False
     return True
 
+def extended_gcd(a, b):
+    if a == 0:
+        return (b, 0, 1)
+    else:
+        g, x, y = extended_gcd(b % a, a)
+        return (g, y - (b // a) * x, x)
+
+def modinv(e, phi_n):
+    g, x, _ = extended_gcd(e, phi_n)
+    if g != 1:
+        raise Exception('Không thể tìm nghịch đảo modulo')
+    else:
+        return x % phi_n
+
 def generateBigPrimeNumbers():
     big_number = int(1)
-    for i in range (1,512):
+    for i in range (1,511):
         big_number += random.randint(0,1) * (1 << i)
+    big_number += (2 ** 511)
     flag = False
     while not flag:
         if Miller_Rabin(big_number):
@@ -49,26 +64,24 @@ def generateBigPrimeNumbers():
         else:
             big_number += 2
     return big_number
-if __name__ == '__main__':
-    
+
+if __name__ == '__main__': 
     p = generateBigPrimeNumbers()
     q = generateBigPrimeNumbers()
-    print(p)
-    print(q)
     N = p*q
     phi_N = (p-1) * (q-1)
-    E = 0
-    for i in range(0,11):
-        E += random.randint(0,9)*(10**i)
-        flag = False;
-        while not flag:
-            if Miller_Rabin(E):
-                flag =True
-            else:
-                E+=2;
-    k = 1
-    d = (k * phi_N + 1)/E
+    random_bits = random.getrandbits(31)
+    E = (1 << 32) | random_bits | 1
+    flag = False;
     while True:
+        if Miller_Rabin(E):
+            flag = True;
+        if flag:
+            break;
+        else:
+            E += 2
+    D = modinv(E,phi_N)
+        
         
         
     
